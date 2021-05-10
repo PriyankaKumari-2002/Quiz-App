e94882079d21b639505673d104954aef3c9ccbee
PriyankaKumari2002 <80983161+PriyankaKumari-2002@users.noreply.github.com>
2021-05-10 21:18:33 +0530

    Quiz App

---
+++ app/src/main/java/com/example/quizapp/answerclass.java
@@ -0,0 +1,42 @@
+package com.example.quizapp;
+
+public class answerclass {
+    private int optionA,optionB,optionC,optionD,questionid,answerid;
+
+    public answerclass(int questionide,int optiona,int optionb,int optionc,int optiond,int answeride)
+    {
+        questionid=questionide;
+        optionA=optiona;
+        questionid=questionide;
+        optionB=optionb;
+        questionid=questionide;
+        optionC=optionc;
+        questionid=questionide;
+        optionD=optiond;
+        answerid=answeride;
+    }
+
+    public int getOptionA() {
+        return optionA;
+    }
+
+    public int getOptionB() {
+        return optionB;
+    }
+
+    public int getOptionC() {
+        return optionC;
+    }
+
+    public int getOptionD() {
+        return optionD;
+    }
+
+    public int getQuestionid() {
+        return questionid;
+    }
+
+    public int getAnswerid() {
+        return answerid;
+    }
+}
